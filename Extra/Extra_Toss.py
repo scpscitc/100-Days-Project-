a8f459385ab313e890b70b937b72bdef23b3af0f
scpscitc <iagtd2007@gmail.com>
2025-04-17 04:44:43 +0600

    Add files via upload

---
+++ Extra/Extra_Toss.py
@@ -0,0 +1,7 @@
+import random
+#heads or tails program!
+coin= random.randint(0,1)
+if coin == 1:
+    print("its heads!")
+elif coin == 0:
+    print("its tails!")
---
+++ Extra/Extra_random bill!.py
@@ -0,0 +1,5 @@
+#random bill!!!
+import random
+print("hehe! guess who's going to pay the bill today?")
+friends = ["Shan", "Sami", "Fahad", "Alve", "Srabon", "Ryan", "Rafid"]
+print(f"Todays bill goes to {random.choice(friends)}")
