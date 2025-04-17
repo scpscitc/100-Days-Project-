77d84a311adf8683dbc769f61081a35b460ec976
scpscitc <iagtd2007@gmail.com>
2025-04-17 06:35:20 +0600

    Update 04. rock paper scissors.py

--- 04. rock paper scissors.py
+++ 04. rock paper scissors.py
@@ -25,27 +25,13 @@ scissors = ("""
 """)
 choice = int(input("O for rock, 1 for paper, 2 for scissors"))
 computer = random.randint(0,2)
-if choice == 0:
-    print("You have chosen")
-    print(rock)
-if choice == 1:
-    print("You have chosen")
-    print(paper)
-if choice == 2:
-    print("You have chosen")
-    print(scissors)
+photos =[rock, paper, scissors]
+if 0<=choice<=2:
+    print(f"You've chosen {photos[choice]}")
 else:
     print("You typed an invalid number! You lose!")
     exit()
-if computer == 0:
-    print("Computer has chosen")
-    print(rock)
-if computer == 1:
-    print("Computer has chosen")
-    print(paper)
-if computer == 2:
-    print("Computer has chosen")
-    print(scissors)
+print(f"Computer has chosen {photos[computer]}")
 if choice == computer:
     print("It's a draw!")
 elif choice == 0 and computer == 1:
